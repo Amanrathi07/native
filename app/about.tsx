254372138e43758634a4cfb6ea328be9c5d4cc22
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-02 23:10:20 +0530

    headershown

--- app/about.tsx
+++ app/about.tsx
@@ -2,7 +2,11 @@ import { View, Text } from 'react-native'
 
 const about = () => {
   return (
-    <View>
+    <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
       <Text>this is about screen</Text>
     </View>
   )
