32bdba0229b6272479774209afd968434b8ed124
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-02 23:05:54 +0530

    added new page

---
+++ app/about.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const about = () => {
+  return (
+    <View>
+      <Text>this is about screen</Text>
+    </View>
+  )
+}
+
+export default about
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,23 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={style.container}
     >
-      <Text>working</Text>
+    
+    <Text>working</Text>
+    <Link href={"/about"}>about screen</Link>
     </View>
   );
 }
+
+
+const style = StyleSheet.create({
+  container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }
+})
