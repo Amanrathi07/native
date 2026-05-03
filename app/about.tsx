e6950cafca67277ddef789fb9ac5d46f400a192e
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-03 22:54:59 +0530

    practic

--- app/about.tsx
+++ app/about.tsx
@@ -1,15 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const about = () => {
   return (
-    <View style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
+    <View style={style.container}>
       <Text>this is about screen</Text>
     </View>
   )
 }
 
+const style = StyleSheet.create({
+  container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }
+})
 export default about
