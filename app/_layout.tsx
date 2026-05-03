b57629d1e1e2434225f42b1a3b5b5414b17f46a3
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-03 22:52:23 +0530

    practic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,6 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return <Stack>
     <Stack.Screen name="index" options={{title:"home"}} />
+    <Stack.Screen name="about" options={{title:"about us"}} />
   </Stack>;
 }
