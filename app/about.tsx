e6f0583bd00a64a1db6b73e7b674d0dc8dea5d2f
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-03 23:03:25 +0530

    working

--- app/about.tsx
+++ app/about.tsx
@@ -1,18 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
 
 const about = () => {
   return (
     <View style={style.container}>
-      <Text>this is about screen</Text>
+      <Text>back</Text>
+      <Text> next</Text>
     </View>
-  )
-}
+  );
+};
 
 const style = StyleSheet.create({
-  container:{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }
-})
-export default about
+  container: {
+    flex: 1,
+    flexDirection:"row",
+    gap:50 ,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default about;
