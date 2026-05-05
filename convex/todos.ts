3498723dc2f28c43f8625816b4af1e4076a25e23
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-05 23:40:42 +0530

    clearall

--- convex/todos.ts
+++ convex/todos.ts
@@ -2,34 +2,57 @@ import { mutation, query } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 
 export const getTodos = query({
-    handler:async (ctx) =>{
-        const todos = await ctx.db.query("todos").order("desc").collect()
-        return todos ;
-    }
+  handler: async (ctx) => {
+    const todos = await ctx.db.query("todos").order("desc").collect();
+    return todos;
+  },
 });
 
 export const addTodos = mutation({
-    args:{text:v.string()},
-    handler:async (ctx,args) =>{
-        const todoId = await ctx.db.insert("todos",{
-            text:args.text ,
-            isCompleted:false
-        })
-    
-        return todoId ;
-    }
-})
+  args: { text: v.string() },
+  handler: async (ctx, args) => {
+    const todoId = await ctx.db.insert("todos", {
+      text: args.text,
+      isCompleted: false,
+    });
 
+    return todoId;
+  },
+});
 
 export const toggleTodo = mutation({
-    args:{id:v.id("todos")},
-    handler:async(ctx,args)=>{
-        const todo = await ctx.db.get(args.id)
-        if(!todo) throw new  ConvexError("todo not found")
-         
-        await ctx.db.patch(args.id,{
-            isCompleted:!todo.isCompleted
-        })    
+  args: { id: v.id("todos") },
+  handler: async (ctx, args) => {
+    const todo = await ctx.db.get(args.id);
+    if (!todo) throw new ConvexError("todo not found");
+
+    await ctx.db.patch(args.id, {
+      isCompleted: !todo.isCompleted,
+    });
+  },
+});
+
+export const updateTodo = mutation({
+  args: { id: v.id("todos"), text: v.string() },
+  handler: async (ctx, args) => {
+    await ctx.db.patch(args.id, {
+      text: args.text,
+    });
+  },
+});
+
+export const deleteTodo = mutation({
+  args: { id: v.id("todos") },
+  handler: async (ctx, arg) => {
+    await ctx.db.delete(arg.id);
+  },
+});
+
+
+export const clearAllTodos = mutation({
+    handler:async (ctx)=>{
+        const todos = await ctx.db.query("todos").collect();
+        for(let i=0;i<todos.length;i++){}
+        await ctx.db.
     }
 })
-
