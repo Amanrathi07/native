e6444890def966096688fe77cd931ff487fe4a16
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-07 23:33:38 +0530

    percentage logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,23 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { api } from '@/convex/_generated/api'
+import { useQuery } from 'convex/react'
+import { createHomeStyles } from '@/assets/styles/home.styles'
+import useTheame from '@/hooks/useTheame'
 
 const Header = () => {
+    const {colors} = useTheame();
+    const homeStyles = createHomeStyles(colors)
+    const todos = useQuery(api.todos.getTodos)
+
+    const completedTodos = todos ? todos.filter(todo=>todo.isCompleted).length : 0 ;
+
+    const totalCount = todos ? todos.length :0 ;
+
+    const progressPercentage = totalCount > 0 ? (completedTodos / totalCount)*100 : 0 ;
+    
   return (
-    <View>
+    <View style={homeStyles.header}>
       <Text>Header</Text>
     </View>
   )
