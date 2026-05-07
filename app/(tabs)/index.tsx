b9a4942fb3fef4ab1220292bc73708c02acae8a5
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-07 23:00:30 +0530

    togel mode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,38 @@
 import { api } from "@/convex/_generated/api";
-import useTheame from "@/hooks/useTheame";
+import useTheame, { ColorScheme } from "@/hooks/useTheame";
 import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-
-
 export default function Index() {
-  const { toggleDarkMode } = useTheame();
+  const { toggleDarkMode, colors } = useTheame();
 
   const todos = useQuery(api.todos.getTodos);
-  console.log(todos)
+  console.log(todos);
 
-  const addTodos = useMutation(api.todos.addTodos)
+  const addTodos = useMutation(api.todos.addTodos);
+  const style = createStyle(colors)
   return (
     <View style={style.container}>
       <Text>working</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Togle Mode</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=>addTodos({text:"todo1"})}>
+      <TouchableOpacity onPress={() => addTodos({ text: "todo1" })}>
         <Text>add todo</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+const createStyle = (colors:ColorScheme) => {
+  const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 22,
+      backgroundColor:colors.bg ,
+    },
+  });
+  return style;
+};
