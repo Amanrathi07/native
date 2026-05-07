4b55bacedc10e4030f42c29ae1cc8afbb4fdc97a
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-07 22:37:27 +0530

    addtodo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
+import { api } from "@/convex/_generated/api";
 import useTheame from "@/hooks/useTheame";
-import { Link } from "expo-router";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+
 
 export default function Index() {
   const { toggleDarkMode } = useTheame();
+
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos)
+
+  const addTodos = useMutation(api.todos.addTodos)
   return (
     <View style={style.container}>
       <Text>working</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Togle Mode</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={()=>addTodos({text:"todo1"})}>
+        <Text>add todo</Text>
       </TouchableOpacity>
     </View>
   );
