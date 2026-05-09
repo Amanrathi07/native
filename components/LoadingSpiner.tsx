e1a1988ceb01935129760bd59d2c43a14dabb558
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-09 23:27:45 +0530

    commpleat

--- components/LoadingSpiner.tsx
+++ components/LoadingSpiner.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import useTheame from "@/hooks/useTheame";
+import { LinearGradient } from "expo-linear-gradient";
+import { ActivityIndicator, Text, View } from "react-native";
 
 const LoadingSpiner = () => {
-  return (
-    <View>
-      <Text>LoadingSpiner</Text>
-    </View>
-  )
-}
+  const { colors } = useTheame();
 
-export default LoadingSpiner
+  const homeStyles = createHomeStyles(colors);
+
+  return (
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <View style={homeStyles.loadingContainer}>
+        <ActivityIndicator size="large" color={colors.primary} />
+        <Text style={homeStyles.loadingText}>Loading your todos...</Text>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default LoadingSpiner;
