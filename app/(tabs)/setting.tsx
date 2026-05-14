04d31122c380473c4697c10d5cfefbf56e8fb014
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-14 23:12:39 +0530

    git add .

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -1,36 +1,47 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
-import React, { useState } from 'react'
-import useTheame from '@/hooks/useTheame'
-import { createSettingsStyles } from '@/assets/styles/setting.styles'
-import { LinearGradient } from 'expo-linear-gradient'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Ionicons } from '@expo/vector-icons'
-import ProgressStats from '@/components/ProgressStats'
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import React, { useState } from "react";
+import useTheame from "@/hooks/useTheame";
+import { createSettingsStyles } from "@/assets/styles/setting.styles";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import ProgressStats from "@/components/ProgressStats";
 
 const setting = () => {
-  const [isAutoSync , setIsAutoSync] = useState(true)
-  const [isNotificationEnable , setIsNotificationEnable] = useState(true)
-  
-  const {colors,isDarkMode,toggleDarkMode} = useTheame() ;
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotificationEnable, setIsNotificationEnable] = useState(true);
 
-  const settingStyles = createSettingsStyles(colors) ;
+  const { colors, isDarkMode, toggleDarkMode } = useTheame();
+
+  const settingStyles = createSettingsStyles(colors);
   return (
-    <LinearGradient colors={colors.gradients.background} style={settingStyles.container} >
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingStyles.container}
+    >
       <SafeAreaView style={settingStyles.safeArea}>
-        <View style={settingStyles.titleContainer}>
-          <LinearGradient colors={colors.gradients.primary} style={settingStyles.iconContainer}>
-            <Ionicons name='settings' size={28} color={"#ff"} />
-          </LinearGradient>
-          <Text style={settingStyles.title}>Settings</Text>
+        <View  style={settingStyles.header}>
+          <View style={settingStyles.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={settingStyles.iconContainer}
+            >
+              <Ionicons name="settings" size={28} color={"#fff"} />
+            </LinearGradient>
+            <Text style={settingStyles.title}>Settings</Text>
+          </View>
         </View>
 
-        <ScrollView style={settingStyles.scrollView} contentContainerStyle={settingStyles.content} showsVerticalScrollIndicator={false}>
+        <ScrollView
+          style={settingStyles.scrollView}
+          contentContainerStyle={settingStyles.content}
+          showsVerticalScrollIndicator={false}
+        >
           <ProgressStats />
         </ScrollView>
       </SafeAreaView>
     </LinearGradient>
-  )
-}
+  );
+};
 
-
-export default setting
+export default setting;
