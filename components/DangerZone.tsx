661704860d363d97e9c360dfcff568f090d2bfa1
Amanrathi07 <amanrathi4321@gmail.com>
2026-05-15 23:54:24 +0530

    delete all todo

--- components/DangerZone.tsx
+++ components/DangerZone.tsx
@@ -1,7 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, Alert } from 'react-native'
 import React from 'react'
+import useTheame from '@/hooks/useTheame'
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 const DangerZone = () => {
+    const {colors} = useTheame() ;
+
+    const clearAllTodos = useMutation(api.todos.clearAllTodos) ;
+
+const handelResetApp = async()=>{
+    Alert.alert("Reset App",
+        "🚨 This will delete All your todos Permanenrly . This action cannot be undone",
+        [
+            {text:"Cancle",style:"cancel"},
+            {
+                text:"Delete All",
+                style:"destructive",
+                onPress:async ()=>{
+                    try {
+                        const result =await clearAllTodos() ;
+                        Alert.alert(`App Reset" , "Successfully deleted ${result.deletedCount} todo${result.deletedCount === 1 ?"":"s"}.Your app has been reset.`)
+                    } catch (error) {
+                        console.log("")
+                    }
+                }
+            }
+        ])
+}
   return (
     <View>
       <Text>DangerZone</Text>
